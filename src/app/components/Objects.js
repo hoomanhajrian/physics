6065ed777f2c9bfdf8929ef88323b656165f255f
Hooman Hajarian <66041489+hoomanhajrian@users.noreply.github.com>
2024-11-16 02:48:43 +0000

    bouncy objects

--- src/app/components/Objects.js
+++ src/app/components/Objects.js
@@ -5,29 +5,32 @@ import { RigidBody } from "@react-three/rapier";
 import CANNON from "cannon";
 
 export const Objects = () => {
+
     return (
         <>
             {/* properties */}
             <OrbitControls />
             <ambientLight intensity={.5} />
-            <directionalLight
+            <spotLight
                 castShadow
-                position={[10, 10, 20]}
+                position={[10, 10, 40]}
                 intensity={5}
+                angle={.8}
+                decay={0}
             />
             {/* objects */}
-            <RigidBody type="fixed">
-                <Box receiveShadow position={[0, 0, 0]} args={[30, 1 , 30]}>
+            <RigidBody position={[0, 0, 0]} type="fixed" >
+                <Box receiveShadow  args={[50, 1 , 50]}>
                     <meshStandardMaterial color={'darkred'} />
                 </Box>
             </RigidBody>
 
-            <RigidBody position={[0, 10, 0]} colliders={'ball'}>
+            <RigidBody position={[0, 10, 0]} colliders={'ball'} restitution={1}>
                 <Sphere castShadow receiveShadow args={[2, 360, 360]} rotation={[0,0,0]}>
                     <meshStandardMaterial color={'blue'} />
                 </Sphere>
             </RigidBody>
-            <RigidBody position={[0, 20, 1.5]} colliders={'cuboid'}>
+            <RigidBody position={[10, 20, 1.5]} colliders={'cuboid'} restitution={.7}>
             <Box castShadow receiveShadow  args={[3, 3, 3]}>
                 <meshStandardMaterial color={'darkGreen'} />
             </Box>
